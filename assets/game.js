6d1bfa89c1828ec65d24d99b410365e5e599bac0
Jieming Liu <jl10@tcl216-10.cs.williams.edu>
2016-01-05 13:28:15 -0500

    demo

--- assets/game.js
+++ assets/game.js
@@ -11,22 +11,44 @@ window.onload = function() {
 
         // Add the containers to our HTML page
         document.getElementById('wsrl-main-display').appendChild(   Game.getDisplay('main').getContainer());
+        document.getElementById('wsrl-avatar-display').appendChild(   Game.getDisplay('avatar').getContainer());
+        document.getElementById('wsrl-message-display').appendChild(   Game.getDisplay('message').getContainer());
     }
 };
 
 var Game = {
   DISPLAYS: {
+    avatar: {
+      w:  20,
+      h: 24,
+      o: null
+    },
     main: {
       w:  80,
       h: 24,
+      o: null
+    },
+    message: {
+      w:  100,
+      h: 6,
       o: null
     }
   },
   init: function(){
     console.log("test");
-    this.DISPLAYS.main.o = new ROT.Display({width:this.DISPLAYS.main.w, height:this.DISPLAYS.main.h});
-    this.renderMain();
+    for (var displayName in this.DISPLAYS){
+      if (this.DISPLAYS.hasOwnProperty(displayName)){
+        this.DISPLAYS[displayName].o = new ROT.Display({width:this.DISPLAYS[displayName].w, height:this.DISPLAYS[displayName].h});
+      }
+    }
+    this.renderAll();
   },
+  renderAll: function(){
+    this.renderMain();
+    this.renderMessage();
+    this.renderAvatar();
+  },
+
   getDisplay: function(displayName){
       return this.DISPLAYS[displayName].o;
   },
@@ -34,5 +56,15 @@ var Game = {
     for (var i = 0; i < 400; i++){
       this.DISPLAYS.main.o.drawText(2, 5+i, "Woot");
     }
-  }
+  },
+  renderMessage: function(){
+    for (var i = 0; i < 400; i++){
+      this.DISPLAYS.message.o.drawText(2, 5+i, "Woot");
+    }
+  },
+  renderAvatar: function(){
+      for (var i = 0; i < 400; i++){
+        this.DISPLAYS.avatar.o.drawText(2, 5+i, "Woot");
+      }
+    }
 };
